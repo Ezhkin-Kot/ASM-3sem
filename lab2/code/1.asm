30386b1b45ab2579725f86a86e3e44b4bcdeaaf9
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-10-04 18:14:48 +0400

    feat: add codes for lab2

---
+++ lab2/code/1.asm
@@ -0,0 +1,65 @@
+.model tiny               
+.code                     
+org 100h  
+
+start:
+  ; Вывод фамилии, имени и номера группы
+	mov DX, offset my_name
+	call out_string
+  call new_line
+  
+  mov AX, 1    ; Занесение первой цифры в регистр AX
+  mov BX, 2    ; Занесение второй цифры в регистр BX
+  ; Перевод цифр в коды соответствующих символов ASCII с помощью команды add
+  add AX, 30h  
+  add BX, 30h    
+
+  ; Вывод первой цифры
+  mov DX, AX
+  call out_char
+
+  ; Вывод пробела 
+  call out_space
+
+  ; Вывод второй цифры 
+  mov DX, BX
+  call out_char
+
+  ; Завершение программы
+	mov AX, 4C00h
+	int 21h
+
+  ; Процедура вывода строки
+	out_string proc
+		mov AH, 09h
+		int 21h
+		ret
+	out_string endp
+
+  ; Процедура вывода символа
+  out_char proc
+    mov AH, 02h
+    int 21h
+    ret
+  out_char endp
+
+  ; Процедура вывода пробела
+  out_space proc
+    mov DL, 00h ; Код пробела в ASCII
+    mov AH, 02h
+    int 21h
+    ret
+  out_space endp
+
+  ; Процедура перехода на новую строку
+  new_line proc
+    mov DX, offset end_line
+    mov AH, 09h
+    int 21h
+    ret
+  new_line endp
+
+;===== Data =====
+my_name db 'Tyumentsev Radomir, 251$'
+end_line db 0Dh, 0Ah, '$' ; Строка с символами перехода на новую строку
+end start
