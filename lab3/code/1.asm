bfcdca0afea457bfcc7164b25383ee91f5302815
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-10-20 23:32:04 +0400

    feat: complete lab3

--- lab3/code/1.asm
+++ lab3/code/1.asm
@@ -1,10 +1,10 @@
 .model small
 .stack 100h
+.386 ; Разрешение трансляции команд процессора 386
 
 .data
 my_name db 'Tyumentsev Radomir, 251', 0Dh, 0Ah, '$'
 
-.386 ; Разрешение трансляции команд процессора 386
 .code
 
 start:
@@ -20,30 +20,30 @@ start:
 	mov BX, 10; Занесение основания системы счисления (делителя)
 	mov CX, 0; Обнуление счётчика
 
-  loop_first: ; Занесение в стек цифр числа
+  divide_loop: ; Занесение в стек цифр числа
     inc  CX; Увеличение счётчика
     mov  DX, 0; Обнуление остатка от деления в DX
     div  BX; Деление AX на BX
     push DX; Занесение остатка от деления в стек
     cmp  AX, 0; Сравнение частного с нулём
-    jne  loop_first; Если AX != 0, то возвращаемся к loop_first
+    jne  divide_loop; Если AX != 0, то возвращаемся к divide_loop
 
   mov AH, 02h ; Занесение в AH кода команды вывода символа
 
-  loop_second: ; Вывод цифр числа из стека
+  print_loop: ; Вывод цифр числа из стека
     pop  DX
-    call out_digit
-    loop loop_second
+    call print
+    loop print_loop
 
   ; Завершение программы
   mov AX, 4C00h
   int 21h
 
   ; Процедура вывода цифры
-  out_digit proc
+  print proc
     add DX, 30h; Перевод цифры в ASCII
     int 21h
     ret
-  out_digit endp
+  print endp
 
 end start
