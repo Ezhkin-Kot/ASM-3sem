144c71d86edc0f93b1751b0f16619318630aac11
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-12-13 18:15:44 +0400

    feat: create calculator (add lab8)

---
+++ lab8/code/1.asm
@@ -0,0 +1,227 @@
+extrn GetStdHandle: proc,
+    lstrlenA: proc,
+    WriteConsoleA: proc,
+    ReadConsoleA: proc,
+    ExitProcess: proc
+
+.data
+STD_OUTPUT_HANDLE equ -11
+STD_INPUT_HANDLE equ -10
+
+hStdInput dq ?
+hStdOutput dq ?
+sum dq ?
+a_prompt db 'a = ', 0
+b_prompt db 'b = ', 0
+sum_message db 'a + b = ', 0
+invalid_char_message db 'Invalid character', 0
+exit_message db 'Press any key to exit...', 0
+new_line db 0Dh, 0Ah, 0
+
+.code
+STACKALLOC macro arg
+  push R15
+  mov R15, RSP
+  sub RSP, 8 * 4
+  if arg
+    sub RSP, 8 * arg
+  endif
+  and SPL, 0F0h
+endm
+
+STACKFREE macro
+  mov RSP, R15
+  pop R15
+endm
+
+NULL_FIFTH_ARG macro
+  mov qword ptr[RSP + 32], 0
+endm
+
+PrintString proc uses RAX RCX RDX R8 R9 R10 R11, string: qword
+  local bytesWritten: qword
+  STACKALLOC 1
+  mov RCX, string
+  call lstrlenA
+  mov RCX, hStdOutput
+  mov RDX, string
+  mov R8, RAX
+  lea R9, bytesWritten
+  NULL_FIFTH_ARG
+  call WriteConsoleA
+  STACKFREE
+  ret 8
+PrintString endp
+
+ReadNumber proc uses RBX RCX RDX R8 R9
+  local readStr[64]: byte, bytesRead: dword
+  STACKALLOC 2
+  mov RCX, hStdInput
+  lea RDX, readStr
+  mov R8, 64
+  lea R9, bytesRead
+  NULL_FIFTH_ARG
+  call ReadConsoleA
+  xor RCX, RCX
+  mov ECX, bytesRead
+  sub ECX, 2; Удаление символов переноса строки и возврата каретки
+  xor RBX, RBX
+  mov R8, 1
+
+  loopString:
+    dec RCX
+    cmp RCX, -1
+    je scanningComplete
+    xor RAX, RAX
+    mov AL, readStr[RCX]
+    cmp AL, '-'
+    jne eval
+    ; Если вначале стоит -, то меняем знак числа
+    neg RBX
+    cmp RCX, 0
+    je scanningComplete
+    jmp error
+
+    eval: 
+      ; Проверка, является ли символ десятичной цифрой
+      cmp AL, 30h
+      jl error
+      cmp AL, 39h
+      jg error
+      sub RAX, 30h
+      mul R8
+      add RBX, RAX
+      mov RAX, 10
+      mul R8
+      mov R8, RAX
+      jmp loopString
+
+    error:
+      lea RAX, invalid_char_message
+      push RAX
+      call PrintString
+      mov R10, 1
+      STACKFREE
+      ret
+
+    scanningComplete:
+      mov R10, 0
+      mov RAX, RBX
+      ; mov sum, RAX
+      STACKFREE
+      ret
+ReadNumber endp
+
+PrintNumber proc uses RAX RCX RDX R8 R9 R10 R11, number: qword
+  local numberStr[22]: byte
+  STACKALLOC 1
+  xor R8, R8
+  mov RAX, number
+  cmp number, 0
+  jge positive
+  ; Число отрицательное
+  mov numberStr[R8], '-'
+  inc R8
+  neg RAX
+
+  positive:
+    mov RBX, 10
+    xor RCX, RCX
+
+    divToDigits:
+      xor RDX, RDX
+      div RBX; RDX:RAX = RAX / RBX
+      add RDX, 30h
+      push RDX
+      inc RCX
+      cmp RAX, 0
+      jne divToDigits
+
+    createString:
+      pop RDX
+      mov numberStr[R8], DL
+      inc R8
+      loop createString
+
+    mov numberStr[R8], 0
+    lea RAX, numberStr
+    push RAX
+    call PrintString
+    ret 8
+PrintNumber endp
+
+
+WaitEnter proc uses RAX RCX RDX R8 R9 R10 R11
+  local readStr:byte, bytesRead:dword
+  STACKALLOC 1
+  
+  ; Выводим сообщение 'Press any key to exit...'
+  mov   RCX, hStdOutput
+  lea   RDX, exit_message
+  mov   R8D, 25 ; Длина строки 'Press any key to exit...'
+  lea   R9, bytesRead
+  NULL_FIFTH_ARG
+  call  WriteConsoleA
+
+  ; Ожидаем ввода одного символа
+  mov   RCX, hStdInput
+  lea   RDX, readStr
+  mov   R8D, 1
+  lea   R9, bytesRead
+  NULL_FIFTH_ARG
+  call  ReadConsoleA
+
+  STACKFREE
+  ret
+WaitEnter endp
+
+mainCRTStartup proc
+  STACKALLOC 0
+
+  mov RCX, STD_OUTPUT_HANDLE
+  call GetStdHandle
+  mov hStdOutput, RAX
+
+  mov RCX, STD_INPUT_HANDLE
+  call GetStdHandle
+  mov hStdInput, RAX
+
+  lea RAX, a_prompt
+  push RAX
+  call PrintString
+
+  call ReadNumber
+  cmp R10, 1; Проверка флага ошибки из ReadNumber
+  je exit_proc; Если ошибка, переход к завершению
+  mov R8, RAX; Сохранение первого числа в R8
+
+  lea RAX, b_prompt
+  push RAX
+  call PrintString
+
+  call ReadNumber
+  cmp R10, 1; Проверка флага ошибки
+  je exit_proc; Если ошибка, переход к завершению
+  
+  add R8, RAX; Прибавление второго числа (в RAX) к первому (в R8)
+
+  lea RAX, sum_message
+  push RAX
+  call PrintString
+
+  mov RAX, R8; Перемещение суммы в RAX для вывода
+  push RAX
+  call PrintNumber
+
+  lea RAX, new_line
+  push RAX
+  call PrintString
+
+  exit_proc:
+    call WaitEnter
+    STACKFREE
+    xor RCX, RCX; Код завершения 0
+    call ExitProcess
+mainCRTStartup endp
+
+end
---
+++ lab8/main.typ
@@ -0,0 +1,28 @@
+#import "conf.typ": conf
+#import "@preview/zebraw:0.5.5": zebraw, zebraw-init, zebraw-themes
+#show: conf.with(
+  title: [Лабораторная работа №8],
+  type: "pract",
+  info: (
+    author: (
+      name: [Тюменцева Радомира Александровича],
+      faculty: [КНиИТ],
+      group: "251",
+      sex: "male",
+    ),
+    inspector: (
+      degree: "Старший преподаватель",
+      name: "Е. М. Черноусова",
+    ),
+  ),
+  settings: (
+    title_page: (
+      enabled: true,
+    ),
+    contents_page: (
+      enabled: false,
+    ),
+  ),
+)
+
+= Задание 8
